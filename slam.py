6f5b28432301f76b6846a7864a2a6e146edb3fc4
gokulba1aji <gokulbalaji05@gmail.com>
2025-11-01 16:49:24 -0500

    testing efficiency of density calculation

--- slam.py
+++ slam.py
@@ -281,8 +281,8 @@ async def websocket_endpoint(websocket: WebSocket):
 
                     kde = KernelDensity(kernel='gaussian', bandwidth=0.2).fit(points_data[high_mask])
                     scores = kde.score_samples(points_data[high_mask])
-                    probs = np.exp(scores)
-                    print("here")
+                    # probs = np.exp(scores)
+                    # print("here")
 
                     # Colors: num_points * 3 * 1 byte (uint8)
                     # print(np.max(data['colors']))
@@ -296,7 +296,7 @@ async def websocket_endpoint(websocket: WebSocket):
                     colors_data[low_mask, 1] = 255
                     colors_data[low_mask, 2] = 255
 
-                    colors_data[high_mask, 0] = 255 * probs
+                    colors_data[high_mask, 0] = 255
                     colors_data[high_mask, 1] = 0
                     colors_data[high_mask, 2] = 0
 
