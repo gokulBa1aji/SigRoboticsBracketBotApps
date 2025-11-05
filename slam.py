75bc7dc58891c4cd39a1f2877b206b6f2b4d7c51
Gokul <gokulbalaji05@gmail.com>
2025-11-04 20:06:25 -0600

    bug fixes

--- slam.py
+++ slam.py
@@ -300,7 +300,7 @@ async def websocket_endpoint(websocket: WebSocket):
                     # print(np.min(data['colors']))
                     # colors_data = data['colors'][:num_points].tobytes() if 'colors' in data.dtype.names else b''
 
-                    M = cv2.getAffineTransform(prev_pointcloud, points_data)
+                    M = cv2.getAffineTransform(prev_pointcloud[:, 0:2], points_data[:, 0:2])
                     print(M)
 
                     # N x 3
