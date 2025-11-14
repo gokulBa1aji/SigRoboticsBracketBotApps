95f1d4c4060dd39b837543891393170b74a360b1
Gokul <gokulbalaji05@gmail.com>
2025-11-13 20:47:05 -0600

    removing prints

--- scheduler.py
+++ scheduler.py
@@ -21,8 +21,8 @@ class Scheduler:
                         self.jobs[i]()
                         end = time.perf_counter()
                         elapsed = end - start
-                        print(self.jobs[i].__name__, ": ")
-                        print(f'Time taken: {elapsed:.6f} seconds')
+                        # print(self.jobs[i].__name__, ": ")
+                        # print(f'Time taken: {elapsed:.6f} seconds')
                     except Exception as e:
                         print(e)
                     t0[i] = t
