8c20739d3c321f1a2572da61dcc85a0a9666f0a3
lie-cpu <141071530+lie-cpu@users.noreply.github.com>
2025-12-02 20:53:33 -0500

    Update slam.py

--- slam.py
+++ slam.py
@@ -369,7 +369,7 @@ async def websocket_endpoint(websocket: WebSocket):
                     high_mask = points_data[:, 2] > 0.05 * range
                     points_data[low_mask, 2] = 20
                     points_data[high_mask, 2] = 0
-                    print(f"{num_points}")
+                    print(num_points)
                     # transform = cv2.getAffineTransform(points_data[:, 0 : 2], prev_pointcloud[:, 0 : 2])
                     # print(transform)
                     # kde = KernelDensity(kernel='gaussian').fit(points_data[high_mask])
